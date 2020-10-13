e4aa9a5157cca493a444a34d2f22e36d24b7d6ed
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-10-12 21:53:38 -0300

    adicionado a landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import logoImg from './images/logo.svg';
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Florianópolis</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          <FiArrowRight className="arrowRight"/>
+        </a>
+      </div>
     </div>
   );
 }
