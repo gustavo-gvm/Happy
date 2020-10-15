24748d6248c915d7e394cbe8cfbb89992c92d424
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-10-15 14:31:25 -0300

    Web: - finalizado pagina de mapa - adicionado paginas CreateOrphanage e Orphanage - criado navegaçao entre as telas

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,19 @@
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
+
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [50, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return (
@@ -24,8 +33,8 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-27.6818398,-48.5087218]}
-        zoom={14}
+        center={[-27.673767, -48.5084822]}
+        zoom={16}
         style={{ width: '100%', height: '100%' }}
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
@@ -33,9 +42,20 @@ function OrphanagesMap() {
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
         />
 
+        <Marker 
+        icon={mapIcon}
+        position={[-27.673767, -48.5084822]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#FFF"/>
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus className="iconPlus"/>
       </Link>
     </div>
